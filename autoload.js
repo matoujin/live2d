f144fa85054ccf0c69f1388c00efab119f27d2ee
matoujin <70650272+matoujin@users.noreply.github.com>
2022-02-27 01:48:41 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
 const live2d_path = 'https://cdn.jsdelivr.net/gh/matoujin/live2d@latest/';
-const tips_path = 'https://cdn.jsdelivr.net/gh/maotujin/live2d@latest/';
+
 // const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
@@ -33,7 +33,7 @@ function loadExternalResource(url, type) {
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
 		initWidget({
-			waifuPath: tips_path + "waifu-tips.json",
+			waifuPath: live2d_path + "waifu-tips.json",
 			apiPath: "https://matoujin.top/api/",
 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
