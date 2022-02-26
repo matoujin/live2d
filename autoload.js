894d4104f3033bff26eb59e9ba35924dfc5bf6c3
matoujin <70650272+matoujin@users.noreply.github.com>
2022-02-26 14:56:51 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/matoujin/cdn@latest/static/live2d";
-const tips_path = "https://cdn.jsdelivr.net/gh/maotujin/cdn@latest/static/live2d";
+const live2d_path = "https://cdn.jsdelivr.net/gh/matoujin/CDN@latest/static/live2d";
+const tips_path = "https://cdn.jsdelivr.net/gh/maotujin/CDN@latest/static/live2d";
 // const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
